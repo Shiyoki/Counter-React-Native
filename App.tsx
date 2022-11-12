c189838bb174f713cdb92141b6180143d5adae32
Shiyoki <shiyokicash1@gmail.com>
2022-11-12 19:11:55 -0400

    Counter-React-Native

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import HolaMundoScreen from './src/screens/HolaMundoScreen';
+import {CounterScreen} from './src/screens/CounterScreen';
+
+
+const App = () => {
+  return (
+    // <HolaMundoScreen />
+    <CounterScreen />
+  );
+};
+
+export default App;
---
+++ src/components/Fab.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
+
+interface Props {
+  title: string;
+  position?: 'br' | 'bl'; // Optional by adding '?'
+  onPress: () => void;
+}
+
+export const Fab = ({title, position = 'br', onPress}: Props) => {
+  const ios = () => {
+    return (
+      <TouchableOpacity
+        onPress={ onPress }
+        activeOpacity={0.8}
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right, // styles depending on position prop
+        ]}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const android = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right, // styles depending on position prop
+        ]}>
+        <TouchableNativeFeedback
+          onPress={onPress}
+          background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+  return Platform.OS === 'ios' ? ios() : android();
+};
+
+const styles = StyleSheet.create({
+  fabLocation: {
+    position: 'absolute',
+    bottom: 25,
+  },
+  left: {
+    left: 25,
+  },
+  right: {
+    right: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+
+    elevation: 10,
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
